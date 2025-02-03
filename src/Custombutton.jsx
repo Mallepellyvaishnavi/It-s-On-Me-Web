47b4bc698a287846531658d4e59eaf6219ee8643
Mallepellyvaishnavi <mallepally.vaishnavi@gmail.com>
2025-02-03 13:46:55 +0530

    second-Error

--- src/Custombutton.jsx
+++ src/Custombutton.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './CustomButton';
+import './CustomButton.css';
 const CustomButton = () => {
   return (
     <button className="custom-button inline-flex items-center text-white bg-[#e63946] border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
