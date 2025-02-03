91859158955bdfa5e8c0dca1ae8ac5cb83aad38c
Mallepellyvaishnavi <mallepally.vaishnavi@gmail.com>
2025-02-03 14:04:11 +0530

    change component name

--- src/AnimatedButton.jsx
+++ src/AnimatedButton.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './AnimatedButton.css'; // Importing the custom button styles
 
-const Button = () => {
-    return (
-      <button className="button">
-        <p>REQUEST A DEMO</p>
-      </button>
-    );
-  };
-  
-  export default Button;
-  
+const AnimatedButton = () => {
+  return (
+    <button className="button">
+      <p>REQUEST A DEMO</p>
+    </button>
+  );
+};
+
+export default AnimatedButton;
