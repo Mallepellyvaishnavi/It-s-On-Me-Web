56b4dd7a9f70620f2fbea109bdf985861a46bbfe
Mallepellyvaishnavi <mallepally.vaishnavi@gmail.com>
2025-02-03 12:01:45 +0530

    changes

---
+++ src/AnimatedButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AnimatedButton.css'; // Importing the custom button styles
+
+const Button = () => {
+    return (
+      <button className="button">
+        <p>REQUEST A DEMO</p>
+      </button>
+    );
+  };
+  
+  export default Button;
+  
